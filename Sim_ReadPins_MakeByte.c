2f0472d3adada68bd457e5dc62fa909f006be00f
Nygel M <nygelmeece8@gmail.com>
2023-02-21 23:24:47 -0500

    first go for real I think

---
+++ Sim_ReadPins_MakeByte.c
@@ -0,0 +1,2 @@
+/* I wanna simulate what reading a PROM. I imagine I'd want to simulate what reading a digital input pin would return.
+   If my first task is more research that's fine, just let me know. */
